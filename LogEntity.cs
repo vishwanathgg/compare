a434354c5f75d39d230f942e28e405d052b8f46c
vishwanathgg <44508340+vishwanathgg@users.noreply.github.com>
2023-02-03 10:53:19 +0000

    Create LogEntity.cs

---
+++ LogEntity.cs
@@ -0,0 +1,8 @@
+    public class Log
+    {
+        public int Number { get; set; }
+        public DateTime? LogTime { get; set; }
+        public string Thread { get; set; }
+        public string LogType { get; set; }
+        public string LogMessage { get; set; }
+    }
